04d9a592369a5c47a05162f39eaab60ad8f2508c
Sam Mueller <sjmueller@gmail.com>
2019-10-02 15:37:32 -0700

    Typo

--- PhoneNumberKit/UI/TextField.swift
+++ PhoneNumberKit/UI/TextField.swift
@@ -12,7 +12,7 @@ import UIKit
 /// Custom text field that formats phone numbers
 open class PhoneNumberTextField: UITextField, UITextFieldDelegate {
 
-    public open var phoneNumberKit = PhoneNumberKit()
+    open var phoneNumberKit = PhoneNumberKit()
 
     /// Override setText so number will be automatically formatted when setting text by code
     override open var text: String? {
